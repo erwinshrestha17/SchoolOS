bd93e4cc02a760e3be0eaa906eb6a57614f7bdbe
Erwin <45751105+erwinshrestha17@users.noreply.github.com>
2026-05-06 19:25:46 +0545

    feat(library-ui): add library copies route

---
+++ apps/web/app/dashboard/library/copies/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { LibraryWorkspace } from '../../../../components/library/library-workspace';
+
+export default function LibraryCopiesPage() {
+  return <LibraryWorkspace initialTab="copies" />;
+}
