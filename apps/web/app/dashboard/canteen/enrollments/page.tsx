b17dc0c994b9d8c42e90b36d11ec28fb3d021197
Erwin <45751105+erwinshrestha17@users.noreply.github.com>
2026-05-06 21:37:23 +0545

    feat: add canteen enrollments page

---
+++ apps/web/app/dashboard/canteen/enrollments/page.tsx
@@ -0,0 +1,5 @@
+import { CanteenWorkspace } from '../../../../components/canteen/canteen-workspace';
+
+export default function CanteenEnrollmentsPage() {
+  return <CanteenWorkspace initialTab="enrollments" />;
+}
