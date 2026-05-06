c74d410f1309ec330846e141885564d275476bbb
Erwin <45751105+erwinshrestha17@users.noreply.github.com>
2026-05-06 21:33:48 +0545

    feat: add transport location page

---
+++ apps/web/app/dashboard/transport/location/page.tsx
@@ -0,0 +1,5 @@
+import { TransportWorkspace } from '../../../../components/transport/transport-workspace';
+
+export default function TransportLocationPage() {
+  return <TransportWorkspace initialTab="location" />;
+}
