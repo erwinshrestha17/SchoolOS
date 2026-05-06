43a7904c4574576cb1d62add65661cdceb7d5fe0
Erwin <45751105+erwinshrestha17@users.noreply.github.com>
2026-05-06 21:38:06 +0545

    feat: add canteen serving page

---
+++ apps/web/app/dashboard/canteen/serving/page.tsx
@@ -0,0 +1,5 @@
+import { CanteenWorkspace } from '../../../../components/canteen/canteen-workspace';
+
+export default function CanteenServingPage() {
+  return <CanteenWorkspace initialTab="serving" />;
+}
