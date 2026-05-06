a32c81fd37d34b9f004d9219b9afef5995c257e5
Erwin <45751105+erwinshrestha17@users.noreply.github.com>
2026-05-06 21:33:16 +0545

    feat: add transport assignments page

---
+++ apps/web/app/dashboard/transport/assignments/page.tsx
@@ -0,0 +1,5 @@
+import { TransportWorkspace } from '../../../../components/transport/transport-workspace';
+
+export default function TransportAssignmentsPage() {
+  return <TransportWorkspace initialTab="assignments" />;
+}
