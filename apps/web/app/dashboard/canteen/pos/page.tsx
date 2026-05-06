7ce9ae48418b4f7d5eeef667f57023916285bfac
Erwin <45751105+erwinshrestha17@users.noreply.github.com>
2026-05-06 21:38:37 +0545

    feat: add canteen pos page

---
+++ apps/web/app/dashboard/canteen/pos/page.tsx
@@ -0,0 +1,5 @@
+import { CanteenWorkspace } from '../../../../components/canteen/canteen-workspace';
+
+export default function CanteenPosPage() {
+  return <CanteenWorkspace initialTab="pos" />;
+}
