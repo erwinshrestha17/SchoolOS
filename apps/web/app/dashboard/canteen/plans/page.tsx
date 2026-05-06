209ef619012cc7e4301b358705cd70e31cb8505e
Erwin <45751105+erwinshrestha17@users.noreply.github.com>
2026-05-06 21:37:08 +0545

    feat: add canteen plans page

---
+++ apps/web/app/dashboard/canteen/plans/page.tsx
@@ -0,0 +1,5 @@
+import { CanteenWorkspace } from '../../../../components/canteen/canteen-workspace';
+
+export default function CanteenPlansPage() {
+  return <CanteenWorkspace initialTab="plans" />;
+}
