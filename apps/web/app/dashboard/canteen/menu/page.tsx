b012d7c8b79d38e8c9b6a2780954ba4449c4fc19
Erwin <45751105+erwinshrestha17@users.noreply.github.com>
2026-05-06 21:36:48 +0545

    feat: add canteen menu page

---
+++ apps/web/app/dashboard/canteen/menu/page.tsx
@@ -0,0 +1,5 @@
+import { CanteenWorkspace } from '../../../../components/canteen/canteen-workspace';
+
+export default function CanteenMenuPage() {
+  return <CanteenWorkspace initialTab="menu" />;
+}
