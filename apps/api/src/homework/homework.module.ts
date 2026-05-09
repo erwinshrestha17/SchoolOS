fae9fd72cbf83f48e15fd41b35e9b9271fcc0aa2
Erwin <45751105+erwinshrestha17@users.noreply.github.com>
2026-05-09 21:21:56 +0545

    feat: register homework attachment access service

--- apps/api/src/homework/homework.module.ts
+++ apps/api/src/homework/homework.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { AuditModule } from '../audit/audit.module';
 import { AuthModule } from '../auth/auth.module';
 import { CommunicationsModule } from '../communications/communications.module';
+import { FileRegistryModule } from '../file-registry/file-registry.module';
+import { HomeworkAttachmentAccessService } from './homework-attachment-access.service';
 import { HomeworkController } from './homework.controller';
 import { HomeworkService } from './homework.service';
 
 @Module({
-  imports: [AuthModule, CommunicationsModule, AuditModule],
+  imports: [AuthModule, CommunicationsModule, AuditModule, FileRegistryModule],
   controllers: [HomeworkController],
-  providers: [HomeworkService],
-  exports: [HomeworkService],
+  providers: [HomeworkService, HomeworkAttachmentAccessService],
+  exports: [HomeworkService, HomeworkAttachmentAccessService],
 })
 export class HomeworkModule {}
