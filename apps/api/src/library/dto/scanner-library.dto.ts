f81255820347d150942c867cf89c9fa1b95533b8
Erwin <45751105+erwinshrestha17@users.noreply.github.com>
2026-06-13 19:47:54 +0545

    feat(library): add scanner workflow DTOs

---
+++ apps/api/src/library/dto/scanner-library.dto.ts
@@ -0,0 +1,38 @@
+import {
+  IsDateString,
+  IsOptional,
+  IsString,
+  ValidateIf,
+} from 'class-validator';
+import { ReturnLibraryCopyDto } from './return-library-copy.dto';
+
+export class ScanLibraryCopyDto {
+  @IsString()
+  code!: string;
+}
+
+export class ScannerIssueLibraryCopyDto {
+  @IsString()
+  code!: string;
+
+  @ValidateIf((dto: ScannerIssueLibraryCopyDto) => !dto.borrowerStaffId)
+  @IsString()
+  borrowerStudentId?: string;
+
+  @ValidateIf((dto: ScannerIssueLibraryCopyDto) => !dto.borrowerStudentId)
+  @IsString()
+  borrowerStaffId?: string;
+
+  @IsOptional()
+  @IsDateString()
+  dueAt?: string;
+
+  @IsOptional()
+  @IsString()
+  notes?: string;
+}
+
+export class ScannerReturnLibraryCopyDto extends ReturnLibraryCopyDto {
+  @IsString()
+  code!: string;
+}
