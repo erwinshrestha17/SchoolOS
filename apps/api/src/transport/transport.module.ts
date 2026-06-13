68eb90ddc49ed89c73acf4d4f34236ec886aaf39
Erwin <45751105+erwinshrestha17@users.noreply.github.com>
2026-06-13 19:46:27 +0545

    Register transport service provider

--- apps/api/src/transport/transport.module.ts
+++ apps/api/src/transport/transport.module.ts
@@ -5,12 +5,13 @@ import { CommunicationsModule } from '../communications/communications.module';
 import { RedisModule } from '../redis/redis.module';
 import { TransportController } from './transport.controller';
 import { TransportHardeningService } from './transport-hardening.service';
+import { TransportM8bService } from './transport-m8b.service';
 import { TransportService } from './transport.service';
 
 @Module({
   imports: [AuthModule, AuditModule, CommunicationsModule, RedisModule],
   controllers: [TransportController],
-  providers: [TransportService, TransportHardeningService],
-  exports: [TransportService, TransportHardeningService],
+  providers: [TransportService, TransportHardeningService, TransportM8bService],
+  exports: [TransportService, TransportHardeningService, TransportM8bService],
 })
 export class TransportModule {}
