31578123a4b32e061cb8684448c818e07da14425
Erwin <45751105+erwinshrestha17@users.noreply.github.com>
2026-06-13 19:51:02 +0545

    feat(hr): wire staff self-service attendance providers

--- apps/api/src/attendance/attendance.module.ts
+++ apps/api/src/attendance/attendance.module.ts
@@ -8,6 +8,11 @@ import { AttendanceCron } from './attendance.cron';
 import { AttendanceController } from './attendance.controller';
 import { AttendanceService } from './attendance.service';
 import { HrAttendanceController } from './hr-attendance.controller';
+import { HrLeaveQueueController } from './hr-leave-queue.controller';
+import { StaffLeaveQueueService } from './staff-leave-queue.service';
+import { StaffSelfServiceController } from './staff-self-service.controller';
+import { StaffSelfServiceService } from './staff-self-service.service';
+import { StaffTimeClockService } from './staff-time-clock.service';
 
 @Module({
   imports: [
@@ -17,8 +22,19 @@ import { HrAttendanceController } from './hr-attendance.controller';
     SettingsModule,
     FileRegistryModule,
   ],
-  controllers: [AttendanceController, HrAttendanceController],
-  providers: [AttendanceService, AttendanceCron],
-  exports: [AttendanceService],
+  controllers: [
+    AttendanceController,
+    HrAttendanceController,
+    HrLeaveQueueController,
+    StaffSelfServiceController,
+  ],
+  providers: [
+    AttendanceService,
+    AttendanceCron,
+    StaffLeaveQueueService,
+    StaffSelfServiceService,
+    StaffTimeClockService,
+  ],
+  exports: [AttendanceService, StaffSelfServiceService, StaffTimeClockService],
 })
 export class AttendanceModule {}
